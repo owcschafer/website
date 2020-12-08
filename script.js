c02c0b033798f390dae6cff9427cfee0d7cc9569
Owen Schafer <owcschafer@hotmail.com>
2020-12-08 02:04:29 +0000

    secret

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function trucks() {
         var trucks = document.getElementById("trucks");
         trucks.play();
       }
+
+function chungus() {
+  open("https://knowyourmeme.com/memes/big-chungus");
+}
